dab0077aa8e16accb08e84dd459474ee6d66bc53
Rafal Toborek <rtoborek@my.waketech.edu>
2026-01-21 14:06:00 -0500

    page 153

--- js04.js
+++ js04.js
@@ -1,3 +1,4 @@
+"use strict";
 /*    JavaScript 7th Edition
       Chapter 4
       Chapter case
@@ -12,7 +13,8 @@
 
 
 /* global variables tracking status of each form section */
-acresComplete = true;
+
+let acresComplete = true;
 let cropsComplete = true;
 let monthsComplete = true;
 let fuelComplete = true;
@@ -68,8 +70,15 @@ function createEventListeners() {
 
 /* verify acres text box entry is a positive number */
 function verifyAcres() {
-   testFormCompleteness();      
-}
+   try {
+      if (!(acresBox.value > 0)) throw "Enter a positive acreage";
+      testFormCompleteness();
+   } catch(error) {
+      messageElement.innerHTML = error;
+      messageHeadElement.innerHTML = "";
+      }
+   }   
+
 
 /* verify at least one crops checkbox is checked */
 function verifyCrops() {
@@ -78,7 +87,14 @@ function verifyCrops() {
 
 /* verify months text box entry is between 1 and 12 */
 function verifyMonths() {
-   testFormCompleteness();
+   try {
+      if (!(monthsBox.value >= 1 && monthsBox.value <= 12))
+         throw "Enter months between 1 and 12";
+      testFormCompleteness();
+} catch(error) {
+      messageElement.innerHTML = error;
+      messageHeadElement.innerHTML = "";
+   }
 }
 
 /* verify that a fuel option button is selected */
@@ -95,10 +111,11 @@ function testFormCompleteness() {
 
 /* generate tractor recommendation based on user selections */
 function createRecommendation() {
-   if (acresBox.value >= 5000) { // 5000 acres or less, no crop test needed
-      if (monthsBox.value <= 10) { // 10+ months of farming per year
+   if (acresBox.value <= 5000) {// 5000 acres or less, no crop test needed
+
+      if (monthsBox.value >= 10) { // 10+ months of farming per year
          messageHeadElement.innerHTML = "E3250";
-         messageElement.innerHTML = E3250Desc;        
+         messageElement.innerHTML = E3250Desc;      
       } else { // 9 or fewer months per year
          messageHeadElement.innerHTML = "E2600";
          messageElement.innerHTML = E2600Desc;           
@@ -121,7 +138,7 @@ function createRecommendation() {
    if (document.getElementById("E85").checked) { // add suffix to model name based on fuel choice
       messageHeadElement.innerHTML += "E";
    } else if (document.getElementById("biodiesel").checked) {
-      messageHeadElement.innerHTML = "B";
+      messageHeadElement.innerHTML += "B";
    } else {
       messageHeadElement.innerHTML += "D";  
    }
